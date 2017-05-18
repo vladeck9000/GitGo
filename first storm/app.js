190e7036b8e5265ffa5e415647d21373353a7a58
vladeck9000 <vladeck.go@gmail.com>
2017-05-18 09:57:56 +0300

    Страница Js

---
+++ first storm/app.js
@@ -0,0 +1,35 @@
+var News = React.createClass({
+    render: function() {
+        return (
+            <div className="news">
+                К сожалению, новостей нет.
+            </div>
+        );
+    }
+});
+
+var App = React.createClass({
+    render: function() {
+        return (
+            <div className="app">
+                Всем привет, я компонент App! Я умею отображать новости.
+                <News />
+                <Comments />
+            </div>
+        );
+    }
+});
+
+var Comments = React.createClass({
+    render: function () {
+        return (<div className="Comments">
+                    Я коментарий
+                </div>
+        );
+    }
+});
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
